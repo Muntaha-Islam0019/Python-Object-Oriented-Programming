f41c96315a0e3a0ac331438963254fe9c697eec8
Muntaha-Islam0019 <muntaha.islam0019@gmail.com>
2022-01-24 18:14:41 +0600

    basic class def

--- Resources/Ch 1/definition_start.py
+++ Resources/Ch 1/definition_start.py
@@ -3,9 +3,16 @@
 
 
 # TODO: create a basic class
+class Book:
+    # Initializer, not constructor
+    def __init__(self, title) -> None:
+        self.title = title
 
 
 # TODO: create instances of the class
-
+b1 = Book("Harry Potter")
+b2 = Book("Feluda")
 
 # TODO: print the class and property
+print(b1)
+print(b1.title)
