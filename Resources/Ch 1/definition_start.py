856a5d167f8fadcb740c008fe47577a84960c1b1
Muntaha-Islam0019 <muntaha.islam0019@gmail.com>
2022-01-25 13:48:14 +0600

    Modified the start files.

--- Resources/Ch 1/definition_start.py
+++ Resources/Ch 1/definition_start.py
@@ -3,16 +3,9 @@
 
 
 # TODO: create a basic class
-class Book:
-    # Initializer, not constructor
-    def __init__(self, title) -> None:
-        self.title = title
 
 
 # TODO: create instances of the class
-b1 = Book("Harry Potter")
-b2 = Book("Feluda")
+
 
 # TODO: print the class and property
-print(b1)
-print(b1.title)
